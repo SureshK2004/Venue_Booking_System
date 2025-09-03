76e2c52c992eb7152fb3900575dc0c262103696a
SureshK2004 <sureshksureshk04gmail.com>
2025-09-03 14:27:30 +0530

    sequelize changed mysql

--- backend/src/config/sequelize.js
+++ backend/src/config/sequelize.js
@@ -10,6 +10,7 @@ export const sequelize = new Sequelize(
     host: process.env.DB_HOST || "localhost",
     port: Number(process.env.DB_PORT || 3306),
     dialect: "mysql",
+    dialectModule: require('mysql2'),
     logging: false,
     timezone: "+00:00",
     pool: {
